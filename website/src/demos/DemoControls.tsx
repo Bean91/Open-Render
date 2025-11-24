e156ba990b1053eadf218a2bfdd95f1d5087738b
Ben Kiev <146984941+Bean91@users.noreply.github.com>
2025-11-24 18:55:52 -0500

    view controls added, stores data in local storage

--- website/src/demos/DemoControls.tsx
+++ website/src/demos/DemoControls.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import "../style.css";
 
 interface request {
@@ -10,13 +10,57 @@ interface request {
 	inputPoints: number[];
 }
 
-const DemoCanvas: React.FC = () => {
-	const [roll, setRoll] = useState<number>(0);
-	const [pitch, setPitch] = useState<number>(0);
-	const [yaw, setYaw] = useState<number>(0);
-	const [x, setX] = useState<number>(0);
-	const [y, setY] = useState<number>(0);
-	const [z, setZ] = useState<number>(0);
+const DemoControls: React.FC = () => {
+	const [requestData, setRequestData] = useState<request[]>(() => {
+		const storedData = localStorage.getItem('requestData');
+		return storedData ? JSON.parse(storedData) : [{type: "line", name: "test", roll: 0, pitch: 0, yaw: 0, inputPoints: [0, 0, 0, 0, 0, 0, 0, 0, 0]}];
+	});
+	const [roll, setRoll] = useState<number>(() => {
+		const storedData = localStorage.getItem('roll');
+		return storedData ? JSON.parse(storedData) : 0;
+	});
+	const [pitch, setPitch] = useState<number>(() => {
+		const storedData = localStorage.getItem('pitch');
+		return storedData ? JSON.parse(storedData) : 0;
+	});
+	const [yaw, setYaw] = useState<number>(() => {
+		const storedData = localStorage.getItem('yaw');
+		return storedData ? JSON.parse(storedData) : 0;
+	});
+	const [x, setX] = useState<number>(() => {
+		const storedData = localStorage.getItem('x');
+		return storedData ? JSON.parse(storedData) : 0;
+	});
+	const [y, setY] = useState<number>(() => {
+		const storedData = localStorage.getItem('y');
+		return storedData ? JSON.parse(storedData) : 0;
+	});
+	const [z, setZ] = useState<number>(() => {
+		const storedData = localStorage.getItem('z');
+		return storedData ? JSON.parse(storedData) : 0;
+	});
+
+	useEffect(() => {
+		localStorage.setItem('requestData', JSON.stringify(requestData));
+	}, [requestData]);
+	useEffect(() => {
+		localStorage.setItem('roll', JSON.stringify(roll));
+	}, [roll]);
+	useEffect(() => {
+		localStorage.setItem('pitch', JSON.stringify(pitch));
+	}, [pitch]);
+	useEffect(() => {
+		localStorage.setItem('yaw', JSON.stringify(yaw));
+	}, [yaw]);
+	useEffect(() => {
+		localStorage.setItem('x', JSON.stringify(x));
+	}, [x]);
+	useEffect(() => {
+		localStorage.setItem('y', JSON.stringify(y));
+	}, [y]);
+	useEffect(() => {
+		localStorage.setItem('z', JSON.stringify(z));
+	}, [z]);
 
 	function changeRoll(event: Event) {
 		setRoll(event.target.value);
@@ -39,92 +83,73 @@ const DemoCanvas: React.FC = () => {
 	}
 
 	function changeRequestType(index: number, e: Event) {
-		const newValue = requestData[index];
-		newValue.type = e.target.value;
-
-		const updatedValues = requestData.map((item, i) =>
-			i === index ? newValue : item
-		);
-
-      setRequestData(updatedValues);
+		const updatedValues = requestData.map((item, i) => {
+			if (i === index) {
+				return { ...item, type: e.target.value };
+			}
+			return item;
+		});
+		setRequestData(updatedValues);
 	}
 
 	function changeRequestName(index: number, e: Event) {
-		const newValue = requestData[index];
-		newValue.name = e.target.value;
-
-		const updatedValues = requestData.map((item, i) =>
-			i === index ? newValue : item
-		);
-
-      setRequestData(updatedValues);
+		const updatedValues = requestData.map((item, i) => {
+			if (i === index) {
+				return { ...item, name: e.target.value };
+			}
+			return item;
+		});
+		setRequestData(updatedValues);
 	}
 
-	function changeRequestPoint(index: number, i: number, e: Event) {
-		const newValue = requestData[index];
-		newValue.inputPoints[i] = e.target.value;
-
-		const updatedValues = requestData.map((item, i) =>
-			i === index ? newValue : item
-		);
-
-      setRequestData(updatedValues);
+	function changeRequestPoint(index: number, pointIndex: number, e: Event) {
+		const updatedValues = requestData.map((item, i) => {
+			if (i === index) {
+				const newInputPoints = item.inputPoints.map((point, pi) => {
+					if (pi === pointIndex) {
+						return e.target.value;
+					}
+					return point;
+				});
+				return { ...item, inputPoints: newInputPoints };
+			}
+			return item;
+		});
+		setRequestData(updatedValues);
 	}
 
 	function changeRequestRoll(index: number, e: Event) {
-		const newValue = requestData[index];
-		newValue.roll = e.target.value;
-
 		const updatedValues = requestData.map((item, i) =>
-			i === index ? newValue : item
+			i === index ? { ...item, roll: e.target.value } : item
 		);
-
-      setRequestData(updatedValues);
+		setRequestData(updatedValues);
 	}
 
 	function changeRequestPitch(index: number, e: Event) {
-		const newValue = requestData[index];
-		newValue.pitch = e.target.value;
-
 		const updatedValues = requestData.map((item, i) =>
-			i === index ? newValue : item
+			i === index ? { ...item, pitch: e.target.value } : item
 		);
-
-      setRequestData(updatedValues);
+		setRequestData(updatedValues);
 	}
 
 	function changeRequestYaw(index: number, e: Event) {
-		const newValue = requestData[index];
-		newValue.yaw = e.target.value;
-
 		const updatedValues = requestData.map((item, i) =>
-			i === index ? newValue : item
+			i === index ? { ...item, yaw: e.target.value } : item
 		);
-
-      setRequestData(updatedValues);
+		setRequestData(updatedValues);
 	}
 
 	function removeObject(index: number) {
-		console.log("delete");
-		setRequestData(data => {
-			data.splice(index, 1);
-			return data;
-		})
-		console.log(requestData);
+		setRequestData(data => data.filter((item, i) => i !== index));
 	}
 
 	function newObject() {
-		console.log("new");
-		setRequestData(data => {
-			data.push({type: "line", name: "", roll: 0, pitch: 0, yaw: 0, inputPoints: [0, 0, 0, 0, 0, 0, 0, 0, 0]});
-			return data;
-		})
-		console.log(requestData);
+		const newItem = {type: "line", name: "", roll: 0, pitch: 0, yaw: 0, inputPoints: [0, 0, 0, 0, 0, 0, 0, 0, 0]};
+		setRequestData(data => [...data, newItem]);
 	}
 
-	const [requestData, setRequestData] = useState<request[]>([{type: "line", name: "test", roll: 0, pitch: 0, yaw: 0, inputPoints: [0, 0, 0, 0, 0, 0, 0, 0, 0]}]);
 	const inputItem = "w-full border border-gray-700 rounded-sm p-1";
-	return (<div id={"controls"} className={"w-1/4 flex flex-col h-child justify-between items-center m-4 p-2 bg-gray-300 rounded-md"}>
+	return (<div id={"controls"} className={"max-h-3/4 scrollbar-thin overflow-x-hidden overflow-y-scroll w-1/4 flex flex-col h-child justify-between items-center m-4 p-2 bg-gray-300 rounded-md"}>
 		<div className={"grid grid-cols-3 gap-2 block w-full border p-1 m-1 rounded-sm block"}>
 			<h1 className={"col-span-3 text-center"}>View Controls</h1>
 			<h3 className={"text-center"}>Roll</h3>
@@ -141,8 +166,6 @@ const DemoCanvas: React.FC = () => {
 			<input className={inputItem} type="number" step={20} value={z} onChange={changeZ} />
 		</div>
 		{requestData.map((val: request, index: number) => {
-			console.log(index);
-			console.log(val);
 			return (
 				<div className={inputItem + " grid gap-2 grid-cols-2 m-2"} key={index}>
 					<select className={inputItem + " col-span-1"} value={val.type} onChange={(e) => changeRequestType(index, e)}>
@@ -157,24 +180,24 @@ const DemoCanvas: React.FC = () => {
 					<div className={inputItem + " grid gap-2 grid-cols-3 col-span-2"}>
 						<h3 className={"col-span-3 text-center"}>Points List</h3>
 						{(val.type === "line" || val.type === "recPrOut" || val.type === "recPrFill") ? <> <input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[0]} onChange={(e) => changeRequestPoint(index, 0, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[1]} onChange={(e) => changeRequestPoint(index, 1, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[2]} onChange={(e) => changeRequestPoint(index, 2, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[3]} onChange={(e) => changeRequestPoint(index, 3, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[4]} onChange={(e) => changeRequestPoint(index, 4, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[5]} onChange={(e) => changeRequestPoint(index, 5, e)} step={20}></input> </> :
-						(val.type === "triangle") ? <> <input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[0]} onChange={(e) => changeRequestPoint(index, 0, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[1]} onChange={(e) => changeRequestPoint(index, 1, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[2]} onChange={(e) => changeRequestPoint(index, 2, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[3]} onChange={(e) => changeRequestPoint(index, 3, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[4]} onChange={(e) => changeRequestPoint(index, 4, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[5]} onChange={(e) => changeRequestPoint(index, 5, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[6]} onChange={(e) => changeRequestPoint(index, 6, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[7]} onChange={(e) => changeRequestPoint(index, 7, e)} step={20}></input>
-													<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[8]} onChange={(e) => changeRequestPoint(index, 8, e)} step={20}></input> </> :
-						(val.type === "circle" || val.type === "sphere") ? <> <input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[0]} onChange={(e) => changeRequestPoint(index, 0, e)} step={20}></input>
-							<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[1]} onChange={(e) => changeRequestPoint(index, 1, e)} step={20}></input>
-							<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[2]} onChange={(e) => changeRequestPoint(index, 2, e)} step={20}></input>
-							<input className={inputItem + " col-span-3"} type={"number"} value={val.inputPoints[3]} onChange={(e) => changeRequestPoint(index, 3, e)} step={20}></input> </> : <></>}
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[1]} onChange={(e) => changeRequestPoint(index, 1, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[2]} onChange={(e) => changeRequestPoint(index, 2, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[3]} onChange={(e) => changeRequestPoint(index, 3, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[4]} onChange={(e) => changeRequestPoint(index, 4, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[5]} onChange={(e) => changeRequestPoint(index, 5, e)} step={20}></input> </> :
+							(val.type === "triangle") ? <> <input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[0]} onChange={(e) => changeRequestPoint(index, 0, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[1]} onChange={(e) => changeRequestPoint(index, 1, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[2]} onChange={(e) => changeRequestPoint(index, 2, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[3]} onChange={(e) => changeRequestPoint(index, 3, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[4]} onChange={(e) => changeRequestPoint(index, 4, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[5]} onChange={(e) => changeRequestPoint(index, 5, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[6]} onChange={(e) => changeRequestPoint(index, 6, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[7]} onChange={(e) => changeRequestPoint(index, 7, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[8]} onChange={(e) => changeRequestPoint(index, 8, e)} step={20}></input> </> :
+							(val.type === "circle" || val.type === "sphere") ? <> <input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[0]} onChange={(e) => changeRequestPoint(index, 0, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[1]} onChange={(e) => changeRequestPoint(index, 1, e)} step={20}></input>
+								<input className={inputItem + " col-span-1"} type={"number"} value={val.inputPoints[2]} onChange={(e) => changeRequestPoint(index, 2, e)} step={20}></input>
+								<input className={inputItem + " col-span-3"} type={"number"} value={val.inputPoints[3]} onChange={(e) => changeRequestPoint(index, 3, e)} step={20}></input> </> : <></>}
 					</div>
 					<div className={inputItem + " grid gap-2 grid-cols-3 col-span-2"}>
 						<h3 className={"col-span-3 text-center"}>Rotations List</h3>
@@ -184,9 +207,10 @@ const DemoCanvas: React.FC = () => {
 					</div>
 					<button className={inputItem + " col-span-2 text-center cursor-pointer"} onClick={() => removeObject(index)}>Delete Object</button>
 				</div>)
-			})}
-		<button className={inputItem + "col-span-3 text-center cursor-pointer"} onClick={newObject}>Add New Object</button>
+		})}
+		<button className={inputItem + " col-span-3 text-center cursor-pointer"} onClick={newObject}>Add New Object</button>
+		<button className={inputItem + " mt-2 col-span-3 text-center cursor-pointer"}>Render</button>
 	</div>);
 }
 
-export default DemoCanvas;
+export default DemoControls;
